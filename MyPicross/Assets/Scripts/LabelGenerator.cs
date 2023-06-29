37cd20d596b72434b125528dee1f2530e8a19531
LITl-l <52400018+LITl-l@users.noreply.github.com>
2023-06-29 11:30:58 +0900

    fix: edit settings

---
+++ MyPicross/Assets/Scripts/LabelGenerator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LabelGenerator : MonoBehaviour
+{
+	[SerializeField] private GameObject board;
+	[SerializeField] private TextMesh _vertical;
+	[SerializeField] private TextMesh _horizontal;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
