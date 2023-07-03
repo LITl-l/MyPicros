dcb44da6a65d50ba3499286899d6fc18e47d183f
LITl-l <fafeeyu@gmail.com>
2023-07-04 00:15:33 +0900

    add: able to Mark on board

--- MyPicross/Assets/Scripts/Cell.cs
+++ MyPicross/Assets/Scripts/Cell.cs
@@ -5,86 +5,81 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Cell : MonoBehaviour
-{
-	private enum PaintMode
-	{
-		None,
-		Paint,
-		Erase,
-		Mark
-	}
-	private static PaintMode _mode;
-	private Image _image;
-	private Sprite _markSprite;
-	private int x;
-	private int y;
-	public (int, int) GetCoord { get { return (x, y); } set { x = value.Item1; y = value.Item2; } }
-	private bool isBlack = false;
-	private bool isMarked = false;
-	private BoardChecker _checker;
+public class Cell : MonoBehaviour {
+  private enum PaintMode {
+    None,
+    Paint,
+    Erase,
+    Mark
+  }
+  private static PaintMode _mode;
+  private Image _image;
+  private Sprite _markSprite;
+  private int x;
+  private int y;
+  public (int, int) GetCoord {
+    get {
+      return (x, y);
+    }
+    set {
+      x = value.Item1;
+      y = value.Item2;
+    }
+  }
+  private bool isBlack = false;
+  private bool isMarked = false;
+  private BoardChecker _checker;
 
-	private async void Start()
-	{
-		_image = GetComponent<Image>();
-		_checker = GameObject.FindWithTag("Board").GetComponent<BoardChecker>();
-		_mode = PaintMode.None;
-		_markSprite = await Addressables.LoadAssetAsync<Sprite>("XMark").Task;
-	}
+  private async void Start() {
+    _image = GetComponent<Image>();
+    _checker = GameObject.FindWithTag("Board").GetComponent<BoardChecker>();
+    _mode = PaintMode.None;
+    _markSprite = await Addressables.LoadAssetAsync<Sprite>("XMark").Task;
+    Addressables.Release(_markSprite);
+  }
 
-	private void OnDestroy()
-	{
-		Addressables.Release(_markSprite);
-	}
+  public void OnPointerDown() {
+    // Debug.Log($"Cell ({x}, {y}) is clicked");
+    if (isBlack)
+      _mode = PaintMode.Erase;
+    else if (isMarked) {
+      if (Input.GetMouseButtonDown(1))
+        _mode = PaintMode.Erase;
+    }
+    else {
+      if (Input.GetMouseButtonDown(1))
+        _mode = PaintMode.Mark;
+      else
+        _mode = PaintMode.Paint;
+    }
+    ColorChange();
+  }
 
-	public void OnPointerDown()
-	{
-		// Debug.Log($"Cell ({x}, {y}) is clicked");
-		if (isBlack)
-			_mode = PaintMode.Erase;
-		else if (isMarked)
-			if (Input.GetMouseButtonDown(1))
-				_mode = PaintMode.Erase;
-			else
-			{
-				if (Input.GetMouseButtonDown(1))
-					_mode = PaintMode.Mark;
-				_mode = PaintMode.Paint;
-			}
-		ColorChange();
-	}
+  public void OnPointerUp() {
+    _mode = PaintMode.None;
+  }
 
-	public void OnPointerUp()
-	{
-		_mode = PaintMode.None;
-	}
+  public void OnPointerEnter() {
+    if (_mode != PaintMode.None)
+      ColorChange();
+  }
 
-	public void OnPointerEnter()
-	{
-		if (_mode != PaintMode.None)
-			ColorChange();
-	}
+  private void ColorChange() {
+    if (_mode == PaintMode.Paint) {
+      _image.color = Color.black;
+      isBlack = true;
+    }
+    else if (_mode == PaintMode.Erase) {
+      _image.sprite = null;
+      _image.color = Color.white;
+      isMarked = false;
+      isBlack = false;
+    }
+    else if (_mode == PaintMode.Mark) {
+      _image.sprite = _markSprite;
+      isMarked = true;
+    }
 
-	private void ColorChange()
-	{
-		if (_mode == PaintMode.Paint)
-		{
-			_image.color = Color.black;
-			isBlack = true;
-		}
-		else if (_mode == PaintMode.Erase)
-		{
-			_image.sprite = null;
-			_image.color = Color.white;
-			isMarked = false;
-			isBlack = false;
-		}
-		else if (_mode == PaintMode.Mark)
-		{
-			_image.sprite = _markSprite;
-			isMarked = true;
-		}
-
-		_checker.OnCellChanged(x, y, isBlack);
-	}
+    _checker.OnCellChanged(x, y, isBlack);
+  }
 }
--- MyPicross/Assets/Scripts/LabelGenerator.cs
+++ MyPicross/Assets/Scripts/LabelGenerator.cs
@@ -6,83 +6,86 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
-public class LabelGenerator : MonoBehaviour
-{
-	[SerializeField] private GameObject board;
-	[SerializeField] private RectTransform _root;
-	[SerializeField] private TextMeshProUGUI _vertical;
-	[SerializeField] private TextMeshProUGUI _horizontal;
+public class LabelGenerator : MonoBehaviour {
+  [SerializeField] private GameObject board;
+  [SerializeField] private RectTransform _root;
+  [SerializeField] private TextMeshProUGUI _vertical;
+  [SerializeField] private TextMeshProUGUI _horizontal;
 
-	private BoardCreator _creator;
-	private ProblemLoader _loader;
-	private int _verticalNum = 0;
-	private int _horizontalNum = 0;
-	private bool[,] _answer;
-	// Start is called before the first frame update
-	async void Start()
-	{
-		CancellationToken ct = destroyCancellationToken;
-		_creator = board.GetComponent<BoardCreator>();
-		_loader = board.GetComponent<ProblemLoader>();
-		_verticalNum = _creator.boardSize;
-		_horizontalNum = _creator.boardSize;
-		// Debug.Log($"h: {_horizontalNum}, v: {_verticalNum}");
-		await CloneLabels(ct);
-	}
+  private BoardCreator _creator;
+  private ProblemLoader _loader;
+  private int _verticalNum = 0;
+  private int _horizontalNum = 0;
+  private bool[,] _answer;
+  // Start is called before the first frame update
+  async void Start() {
+    CancellationToken ct = destroyCancellationToken;
+    _creator = board.GetComponent<BoardCreator>();
+    _loader = board.GetComponent<ProblemLoader>();
+    _verticalNum = _creator.boardSize;
+    _horizontalNum = _creator.boardSize;
+    // Debug.Log($"h: {_horizontalNum}, v: {_verticalNum}");
+    await CloneLabels(ct);
+  }
 
-	private async UniTask CloneLabels(CancellationToken token)
-	{
-		_answer = await _loader.LoadProblem(token);
-		Util.printArray(_answer, "in label");
+  private async UniTask CloneLabels(CancellationToken token) {
+    _answer = await _loader.LoadProblem(token);
+    Util.printArray(_answer, "in label");
 
-		GenerateLabels(_vertical, true);
-		GenerateLabels(_horizontal, false);
-	}
+    GenerateLabels(_vertical, true);
+    GenerateLabels(_horizontal, false);
+  }
 
-	private void GenerateLabels(TextMeshProUGUI label, bool isVertical)
-	{
-		int num = isVertical ? _verticalNum : _horizontalNum;
-		TextMeshProUGUI origin = isVertical ? _vertical : _horizontal;
+  private void GenerateLabels(TextMeshProUGUI label, bool isVertical) {
+    int num = isVertical ? _verticalNum : _horizontalNum;
+    TextMeshProUGUI origin = isVertical ? _vertical : _horizontal;
 
-		List<string> labelTexts = new List<string>();
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < num; i++)
-		{
-			int count = 0;
-			int maxNum = isVertical ? _horizontalNum : _verticalNum;
-			sb.Length = 0;
-			for (int j = 0; j < maxNum; j++)
-			{
-				bool state = isVertical ? _answer[i, j] : _answer[j, i];
-				if (state)
-					count++;
-				else
-				{
-					if (count > 0)
-					{
-						sb.Append(count);
-						if (isVertical)
-							sb.Append("  ");
-						else sb.Append("\n");
-					}
-					count = 0;
-				}
-			}
-			if (sb.Length == 0) sb.Append("0");
-			labelTexts.Add(sb.ToString());
-		}
+    List<string> labelTexts = new List<string>();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < num; i++) {
+      int count = 0;
+      int maxNum = isVertical ? _horizontalNum : _verticalNum;
+      sb.Length = 0;
+      for (int j = 0; j < maxNum; j++) {
+        bool state = isVertical ? _answer[i, j] : _answer[j, i];
+        if (!isVertical)
+          Debug.Log($"i: {i}, j: {j}, state: {state}");
+        if (state)
+          count++;
+        else {
+          if (count > 0) {
+            sb.Append(count);
+            if (isVertical)
+              sb.Append("  ");
+            else
+              sb.Append("\n");
+          }
+          count = 0;
+        }
+      }
+      if (count > 0) {
+        sb.Append(count);
+        if (isVertical)
+          sb.Append("  ");
+        else
+          sb.Append("\n");
+      }
+      if (sb.Length == 0)
+        sb.Append("0");
+      labelTexts.Add(sb.ToString());
+    }
 
-		if (isVertical)
-			_vertical.text = labelTexts[0];
-		else _horizontal.text = labelTexts[0];
+    if (isVertical)
+      _vertical.text = labelTexts[0];
+    else
+      _horizontal.text = labelTexts[0];
 
-		for (int i = 1; i < num; i++)
-		{
-			var newLabel = Instantiate(label, _root, true);
-			float positonX = isVertical ? origin.rectTransform.anchoredPosition.x : 10 + 25 * i + 5 * i;
-			float positonY = isVertical ? -20 - 25 * i - 5 * i : origin.rectTransform.anchoredPosition.y;
-			newLabel.rectTransform.anchoredPosition = new Vector2(positonX, positonY);
-			newLabel.text = labelTexts[i];
-		}
-	}
+    for (int i = 1; i < num; i++) {
+      var newLabel = Instantiate(label, _root, true);
+      float positonX = isVertical ? origin.rectTransform.anchoredPosition.x : 10 + 25 * i + 5 * i;
+      float positonY = isVertical ? -20 - 25 * i - 5 * i : origin.rectTransform.anchoredPosition.y;
+      newLabel.rectTransform.anchoredPosition = new Vector2(positonX, positonY);
+      newLabel.text = labelTexts[i];
+    }
+  }
 }
