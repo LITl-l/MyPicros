383a19ca5d677b93d507c18657c9c4bb0bc7d58e
LITl-l <52400018+LITl-l@users.noreply.github.com>
2023-07-04 22:47:50 +0900

    tempush

--- MyPicross/Assets/Scripts/Util.cs
+++ MyPicross/Assets/Scripts/Util.cs
@@ -4,55 +4,85 @@ using UnityEngine;
 
 public class Util : MonoBehaviour
 {
-    public static void Rotate<T>(T[,] array)
-    {
-        int width = array.GetLength(0);
-        int height = array.GetLength(1);
+	/// <summary>
+	/// ??????????(array)????????(degree)?
+	/// 90ｰ?????????????Rotate()???????????????
+	/// </summary>
+	/// <param name="array"></param>
+	/// <param name="degree"></param>
+	/// <typeparam name="T"></typeparam>
+	public static void RotateOptional<T>(T[,] array, int degree)
+	{
+		if (degree < 0)
+			degree = 360 + degree;
+		//90ｰ???????
+		int normalizedDegree = degree % 360;
+		// Debug.Log($"normalized{normalizedDegree}");
+		//????
+		int rotation = normalizedDegree / 90;
+		// Debug.Log($"rotating{rotation}");
 
-        T[,] copy = (T[,])array.Clone();
+		for (int i = 0; i < rotation; i++)
+		{
+			Rotate(array);
+		}
+		// printArray(array, "Rotation");
+	}
 
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                array[x, y] = copy[height - 1 - y, x];
-            }
-        }
-    }
+	/// <summary>
+	/// ??????????(array)??????90ｰ????
+	/// </summary>
+	/// <param name="array"></param>
+	/// <typeparam name="T"></typeparam>
+	public static void Rotate<T>(T[,] array)
+	{
+		int width = array.GetLength(0);
+		int height = array.GetLength(1);
 
-    public static void ReverseHorizontal<T>(T[,] array)
-    {
-        int width = array.GetLength(0);
-        int height = array.GetLength(1);
+		T[,] copy = (T[,])array.Clone();
 
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width / 2; x++)
-            {
-                T temp = array[x, y];
-                array[x, y] = array[width - 1 - x, y];
-                array[width - 1 - x, y] = temp;
-            }
-        }
-    }
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				array[x, y] = copy[height - 1 - y, x];
+			}
+		}
+	}
 
-    public static void printArray<T>(T[,] array, string name = "")
-    {
-        string output = name + ":\n";
-        int rows = array.GetLength(0);
-        int columns = array.GetLength(1);
+	public static void ReverseHorizontal<T>(T[,] array)
+	{
+		int width = array.GetLength(0);
+		int height = array.GetLength(1);
 
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                output += array[i, j] + "\t"; // タブ区切りで表示する場合
-                                              // output += array[i, j] + " "; // スペース区切りで表示する場合
-            }
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width / 2; x++)
+			{
+				T temp = array[x, y];
+				array[x, y] = array[width - 1 - x, y];
+				array[width - 1 - x, y] = temp;
+			}
+		}
+	}
 
-            output += "\n"; // 改行
-        }
+	public static void printArray<T>(T[,] array, string name = "")
+	{
+		string output = name + ":\n";
+		int rows = array.GetLength(0);
+		int columns = array.GetLength(1);
 
-        Debug.Log(output);
-    }
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				output += array[i, j] + "\t"; // タブ区旙りで表示する晝插
+																			// output += array[i, j] + " "; // スペ拏ス区旙りで表示する晝插
+			}
+
+			output += "\n"; // 改捏
+		}
+
+		Debug.Log(output);
+	}
 }
